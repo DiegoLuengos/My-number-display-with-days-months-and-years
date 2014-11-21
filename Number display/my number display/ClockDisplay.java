042316a5b66cc33906170970f24d3b7ee153d0c9
Diego <klosey88@hotmail.com>
2014-11-21 14:23:58 +0100

    añadidos dias, meses y años

--- Number display/my number display/ClockDisplay.java
+++ Number display/my number display/ClockDisplay.java
@@ -11,7 +11,13 @@ public class ClockDisplay
     private NumberDisplay hours;
     // Un objeto NumberDisplay que nos guarda los minutos.
     private NumberDisplay minutes;
-    // Un String de 5 caracteres: la hora, dos puntos y los minutos.
+    // Un objeto NumberDisplay que nos guarda el dia.
+    private NumberDisplay days;
+    //Un objeto NumberDisplay que nos guarda los meses.
+    private NumberDisplay months;
+    //Un objeto NumberDisplay que nos guarda los años.
+    private NumberDisplay years;
+    // Un String de 16 caracteres: la hora, dos puntos, los minutos y los días, barra, los meses, barra, los años.
     private String displayString;
 
     /**
@@ -21,31 +27,43 @@ public class ClockDisplay
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
+        days = new NumberDisplay(30);
+        months = new NumberDisplay(12);
+        years = new NumberDisplay(99);
         updateDisplay();
     }
     /**
-     * Constructor que crea un reloj con las horas y los minutos dados.
+     * Constructor que crea un reloj con las horas, los minutos, los días, los meses y los años dados.
      */
-    public ClockDisplay(int newHours, int newMinutes)
+    public ClockDisplay(int newHours, int newMinutes, int newDays, int newMonths, int newYears)
     {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
+        days = new NumberDisplay(31);
+        months = new NumberDisplay(13);
+        years = new NumberDisplay(100);
         hours.setValue(newHours);
         minutes.setValue(newMinutes);
+        days.setValue(newDays);
+        months.setValue(newMonths);
+        years.setValue(newYears);
         updateDisplay();
     }
     /**
-     * Fija una nueva hora a partir de las horas y minutos dados.
+     * Fija una nueva hora y fecha a partir de las horas, minutos, días, meses y años dados.
      */
-    public void setTime(int actualHours, int actualMinutes)
+    public void setTime(int actualHours, int actualMinutes, int actualDays, int actualMonths, int actualYears)
     {
         hours.setValue(actualHours);
         minutes.setValue(actualMinutes);
+        days.setValue(actualDays);
+        months.setValue(actualMonths);
+        years.setValue(actualYears);
         updateDisplay();
     }
     /**
-     * Devuelve una cadena de 5 caracteres con la hora y los minutos
-     * separados por dos puntos.
+     * Devuelve una cadena de 16 caracteres con la hora y los minutos separados por dos puntos
+     * y los días, los meses y los años separados por barras.
      */
     public String getTime()
     { 
@@ -60,28 +78,39 @@ public class ClockDisplay
         minutes.increment();
         if (minutes.getValue() == 0){
             hours.increment();
+            if (hours.getValue() == 0){
+                days.increment();
+                if (days.getValue() ==0 ){
+                    days.increment();
+                    months.increment();
+                    if (months.getValue() == 0){
+                        months.increment();
+                        years.increment();
+                    }
+            }
         }
         updateDisplay();
+    }
     }
     /**
      * Actualiza el atributo displayString.
      */
     private void updateDisplay()
     {
-        displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
+        displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + " " + days.getDisplayValue() + "/" + months.getDisplayValue() + "/" + years.getDisplayValue();
         if (hours.getValue() < 12){
-            displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + "am";
+            displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + " "  + "am"  + " " + days.getDisplayValue() + "/" + months.getDisplayValue() + "/" + years.getDisplayValue();
             if (hours.getValue() == 0){
-                displayString = 12 + ":" + minutes.getDisplayValue() + "am";
+                displayString = 12 + ":" + minutes.getDisplayValue() + "am" + " " + days.getDisplayValue() + "/" + months.getDisplayValue() + "/" + years.getDisplayValue();;
             }
         }
         else if (hours.getValue() == 12){
-            displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + "pm";
+            displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue() + "pm" + " " + days.getDisplayValue() + "/" + months.getDisplayValue() + "/" + years.getDisplayValue();
             }
         
         else{
             int otherHours = hours.getValue() -12;
-            displayString = otherHours + ":" + minutes.getDisplayValue() + "pm";
+            displayString = otherHours + ":" + minutes.getDisplayValue() + "pm" + " "  + days.getDisplayValue() + "/" + months.getDisplayValue() + "/" + years.getDisplayValue();
             }  
     }
 
